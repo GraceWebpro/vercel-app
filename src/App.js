cfc7b792d1a9639b60c195d0e1908a8b62fa8721
gracewebpro <gracewebpro@gmail.com>
2022-09-10 03:50:45 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Header from './components/header/Header';
+import Music from './components/music/Music';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router forceRefresh={true}>
+        <Header />
+        <Switch>
+          <Route exact path='/music' component={Music}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
